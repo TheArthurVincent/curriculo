aa303faa71eea3e779248fe537dd8e3fe61605e0
TheArthurVincent <arthurcardosocorp@gmail.com>
2022-12-30 11:21:01 -0300

    #correção animação números

--- js/experiencia.js
+++ js/experiencia.js
@@ -6,15 +6,17 @@ let experiencias = document.querySelectorAll(".aexperiencia");
 // anima números
 const numeros = document.querySelectorAll(".ano-experiencia");
 
-numeros.forEach((numero) => {
-  const anoFinal = numero.innerHTML;
-  let start = 1900;
-  const timer = setInterval(() => {
-    start = start + 1;
-    numero.innerHTML = start;
-    if (start > anoFinal) clearInterval(timer);
-  }, 0);
-});
+function rodarNumero() {
+  numeros.forEach((numero) => {
+    const anoFinal = numero.innerHTML;
+    let start = 1900;
+    const timer = setInterval(() => {
+      start++;
+      numero.innerHTML = start;
+      if (start >= anoFinal) clearInterval(timer);
+    }, 0);
+  });
+}
 
 // ativar
 function ativar(index) {
@@ -22,10 +24,19 @@ function ativar(index) {
     experiencia.classList.replace("ativo", "inativo");
   });
   experiencias[index].classList.replace("inativo", "ativo");
+  rodarNumero();
 }
 
 imagens.forEach((imagem, posicaoDaImagem) => {
   imagem.addEventListener("click", () => {
     ativar(posicaoDaImagem);
   });
+});
+var doneYet = false;
+
+window.addEventListener("scroll", (event) => {
+  if (window.scrollY > 300 && !doneYet) {
+    doneYet = true;
+    rodarNumero();
+  }
 });
