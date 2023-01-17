c086f01c51e1b3255a6a1bdeab937163cb97a52f
TheArthurVincent <arthurcardosocorp@gmail.com>
2023-01-17 09:31:16 -0300

    #mudança pastas

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initExperiencia from "./experiencia.js";
+initExperiencia();
+
+import initCInter from "./c-inter.js";
+initCInter();
+
+import CTec from "./c-tec.js";
+CTec();
+
+import initLinguas from "./linguas.js";
+initLinguas();
